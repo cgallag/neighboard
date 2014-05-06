6ceac8c841fb5b83ffd8c0073f84d4299580ce0c
Caroline Gallagher <cgallagher@wellesley.edu>
2014-05-06 16:50:53 -0400

    Board panels now working. Now I need to add posts.

--- NeighBoardPosts.cgi
+++ NeighBoardPosts.cgi
@@ -8,7 +8,8 @@ import boardData
 if __name__ == '__main__':
 	print 'Content-type: text/html\n'
 
-	names = boardData.getBoards()
+	names = boardData.getBoardNames()
+	[boards_col1, boards_col2] = boardData.displayBoards()
 	tmpl = cgi_utils_sda.file_contents('NeighBoard_Home.html')
-	page = tmpl.format(boardnames=names)
+	page = tmpl.format(boardnames=names, first_col_boards = boards_col1, second_col_boards = boards_col2)
 	print page
--- boardData.py
+++ boardData.py
@@ -4,7 +4,8 @@ import MySQLdb
 from cgallag2_dsn import DSN 
 import dbconn
 
-def getBoards():
+
+def getBoardNames():
 	DSN['database'] = 'cgallag2_db'
 	conn = dbconn.connect(DSN)
 	
@@ -17,12 +18,47 @@ def getBoards():
 			return "\n".join(names)
 		names.append("<li id=\"{boardId}-nav\"><a href=\"#\">{name}</a></li>".format(**row))
 
+
+def displayBoards():
+	DSN['database'] = 'cgallag2_db'
+	conn = dbconn.connect(DSN)
+	
+	curs = conn.cursor(MySQLdb.cursors.DictCursor)
+	curs.execute("select boardId, name from board where type='board'");
+	total_boards = curs.rowcount
+	curs.execute("select boardId, name from board where type='board'");
+	boards_printed = 0
+	boards_1 = []
+	boards_2 = []
+
+	panel_html = """<div class=\"panel panel-default\">
+				<div class=\"panel-heading\">{name}<span class=\"badge pull-right\">10</span></div>
+				<div class=\"list-group\" id=\"{boardId}-board\">
+				</div>
+                </div>"""
+
+	while True:
+		row = curs.fetchone()
+
+		if (boards_printed < total_boards/2):
+			boards_1.append(panel_html.format(**row))
+
+		else:
+			if row == None:
+				return ["\n".join(boards_1), "\n".join(boards_2)]
+			boards_2.append(panel_html.format(**row))
+
+		boards_printed += 1
+
+
 def main():
-	boards = getBoards()
-	return boards
+	names = getBoardNames()
+	[boards_col1, boards_col2] = displayBoards()
+	return names, boards_col1, boards_col2
 
 if __name__ == "__main__":
-	print main()
+	for each in main():
+		print each
 
 
 
