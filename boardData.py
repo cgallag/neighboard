d88e6c08e4fac552dc403f9463673af34fe368c9
Caroline Gallagher <cgallagher@wellesley.edu>
2014-05-06 15:51:44 -0400

    Getting board names from database

--- boardData.py
+++ boardData.py
@@ -9,8 +9,20 @@ def getBoards():
 	conn = dbconn.connect(DSN)
 	
 	curs = conn.cursor(MySQLdb.cursors.DictCursor)
-	curs.execute('select ')
+	curs.execute("select name from board where type='board'");
+	names = []
+	while True:
+		row = curs.fetchone()
+		if row == None:
+			return "\n"
+		lines.append("<li id=\"{name}-nav\"><a href=\"#\">{name}</a></li>")
 
+def main():
+	boards = getBoards()
+	return boards
+
+if __name__ == "__main__":
+	print main()
 
 
 
