d7e01bb72e61116a17585917bfa261055b10bab1
Caroline Gallagher <cgallagher@wellesley.edu>
2014-05-15 20:01:29 -0400

    Set up images

--- NeighBoardPosts.cgi
+++ NeighBoardPosts.cgi
@@ -3,77 +3,9 @@
 import sys
 import cgi
 import cgitb; cgitb.enable()
-import re
 
 import cgi_utils_sda
 import boardData
-
-DEST_DIR = '/home/neighbrd/public_html/images/'
-DEST_URL = '/~neighbrd/images/'
-IN_DB    = False                # false means store in dest_dir
-MAX_FILE_SIZE = 100000          # 100 KB
-# ======================================================================
-
-# Code for adding photos to database
-def generate_actor_menu(cursor):
-    '''Returns an HTML menu of all actors in the WMDB'''
-    cursor.execute('SELECT nm,name FROM person ORDER BY name')
-    menu = '<select name="nm">\n'
-    for row in cursor.fetchall():
-        menu += '<option value="%s">%s</option>\n' % row
-    menu += '</select>\n'
-    return menu
-
-def filesize(absfilename):
-    '''Returns the length of a file in bytes'''
-    stat = os.stat(absfilename)
-    return stat.st_size
-
-def check_integer(string,default):
-    '''Converts string to an integer if it's all digits, otherwise
-returns default'''
-    if re.search('^\\d+$',string):
-        try:
-            return int(string)
-        except:
-            return default
-    else:
-        return default
-
-def store_data_in_filesystem_paranoid(nm,client_filename,file_data,cursor):
-    '''Stores data checking for lots of errors'''
-    dest_file = DEST_DIR + str(nm) + '.jpg'
-    try:
-        stream = open(dest_file,'wb')
-    except Exception as e:
-        return 'Failure to open output file %s: %s' % (dest_file,e)
-    try:
-        stream.write(file_data)
-    except Exception as e:
-        return 'Failure to copy file data to %s: %s' % (dest_file,e)
-    try:
-        os.chmod(dest_file,0644)
-    except Exception as e:
-        return 'Failure to make file %s world-readable: %s' % (dest_file,e)
-
-    ## Now, record the URL in the database
-    url = DEST_URL + str(nm) + '.jpg'
-    try:
-        ## inserts or updates picture blob for this actor
-        rows_mod = cursor.execute('''
-INSERT INTO picfile(nm,url) VALUES (%s,%s)
-ON DUPLICATE KEY UPDATE url=%s
-''',
-                                  (nm,url,url))
-    except Exception as e:
-        print e
-        return 'Failure to store picture URL in database: '+str(e)
-    if rows_mod != 1:
-        return ('Failure to store picture URL in database; '+
-                'rows modified is '+
-                str(rows_mod))
-    return ('The picture file %s was uploaded successfully as %s (%s) ' %
-            (client_filename,dest_file,url))
 
 
 if __name__ == '__main__':
@@ -126,7 +58,7 @@ if __name__ == '__main__':
 		    post_values['image'] = cgi.escape(form_data.getfirst('new-post-image'))
 
 		feedback = boardData.addPost(post_values['recipients'], post_values['subject'], 
-			post_values['message'], post_values['tags'])
+			post_values['message'], post_values['tags'], post_values['image'])
 
 
 
--- boardData.py
+++ boardData.py
@@ -2,210 +2,330 @@
 
 import math
 from datetime import datetime
+import re
+import os
 
 import MySQLdb
 from neighbrd_dsn import DSN
 import dbconn
 
+DEST_DIR = '/home/neighbrd/public_html/images/'
+DEST_URL = '/~neighbrd/images/'
+MAX_FILE_SIZE = 100000          # 100 KB
+# ======================================================================
+
+# Code for adding photos to database
+def filesize(absfilename):
+    '''Returns the length of a file in bytes'''
+    stat = os.stat(absfilename)
+    return stat.st_size
+
+
+def check_integer(string, default):
+    '''Converts string to an integer if it's all digits, otherwise
+returns default'''
+    if re.search('^\\d+$', string):
+        try:
+            return int(string)
+        except:
+            return default
+    else:
+        return default
+
+
+def store_data_in_filesystem_paranoid(postId, client_filename, file_data,
+                                      cursor):
+    '''Stores data checking for lots of errors'''
+    dest_file = DEST_DIR + str(postId) + '.jpg'
+    try:
+        stream = open(dest_file, 'wb')
+    except Exception as e:
+        return 'Failure to open output file %s: %s' % (dest_file, e)
+    try:
+        stream.write(file_data)
+    except Exception as e:
+        return 'Failure to copy file data to %s: %s' % (dest_file, e)
+    try:
+        os.chmod(dest_file, 0644)
+    except Exception as e:
+        return 'Failure to make file %s world-readable: %s' % (dest_file, e)
+
+    ## Now, record the URL in the database
+    url = DEST_URL + str(postId) + '.jpg'
+    try:
+        ## inserts or updates picture blob for this actor
+        rows_mod = cursor.execute('''
+INSERT INTO picfile(postId,picUrl) VALUES (%s,%s)
+ON DUPLICATE KEY UPDATE picUrl=%s
+''',
+                                  (postId, url, url))
+    except Exception as e:
+        print e
+        return 'Failure to store picture URL in database: ' + str(e)
+    if rows_mod != 1:
+        return ('Failure to store picture URL in database; ' +
+                'rows modified is ' +
+                str(rows_mod))
+    return ('The picture file %s was uploaded successfully as %s (%s) ' %
+            (client_filename, dest_file, url))
+
+
+def process_file_upload(postId, client_filename, local_file, cursor):
+    ## Test if the file was uploaded
+    if not client_filename:
+        return 'No file uploaded (yet)'
+
+    file_data = local_file.read()
+    ## Double check whether the file upload is too big
+    if len(file_data) > MAX_FILE_SIZE:
+        return 'Uploaded file is too big: ' + str(len(file_data))
+
+    ## Get the postId, which we will either use as a DB key or a filename
+    postId = check_integer(postId, None)
+    if postId == None:
+        return 'postId has illegal value: %s' % postId
+
+    return store_data_in_filesystem_paranoid(postId, client_filename,
+                                             file_data, cursor)
+
+
+def add_image(conn, boardId, current_time, filename, filedata):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+
+    curs.execute(
+        "select formId from form where boardId=%s and created=%s and type='post'",
+        (boardId, current_time))
+
+    post_row = curs.fetchone()
+    if post_row is not None:
+        postId = post_row['formId']
+        process_file_upload(postId, filename, filedata, curs)
+
+
+def display_image(conn, postId):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute("select * from picfile where postId=%s", (postId,))
+
+    post_row = curs.fetchone()
+    picUrl = post_row['picUrl']
+    if post_row is not None:
+        return "<img alt={postId}-img src={picUrl}>".format(postId=postId,
+                                                           picUrl=picUrl)
+
 
 def getBoardNames():
-	conn = dbconn.connect(DSN)
-	
-	curs = conn.cursor(MySQLdb.cursors.DictCursor)
-	curs.execute("select boardId, name from board where type='board'");
-	names = []
-	while True:
-		row = curs.fetchone()
-		if row == None:
-			return "\n".join(names)
-		names.append("<li id=\"{boardId}-nav\"><a href=\"#\">{name}</a></li>".format(**row))
+    conn = dbconn.connect(DSN)
+
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute("select boardId, name from board where type='board'");
+    names = []
+    while True:
+        row = curs.fetchone()
+        if row == None:
+            return "\n".join(names)
+        names.append(
+            "<li id=\"{boardId}-nav\"><a href=\"#\">{name}</a></li>".format(
+                **row))
 
 
 def displayBoards():
-	conn = dbconn.connect(DSN)
-	
-	curs = conn.cursor(MySQLdb.cursors.DictCursor)
-	curs.execute("select boardId, name, mailname from board where type='board'");
-	total_boards = curs.rowcount
-	
-	boards_printed = 0
-	boards_1 = []
-	boards_2 = []
+    conn = dbconn.connect(DSN)
 
-	panel_html_heading = """
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute(
+        "select boardId, name, mailname from board where type='board'");
+    total_boards = curs.rowcount
+
+    boards_printed = 0
+    boards_1 = []
+    boards_2 = []
+
+    panel_html_heading = """
 		<div class=\"panel panel-default\">
 			<div class=\"panel-heading\">{name}<span class=\"badge pull-right\">"""
 
-	panel_html_posts = """</span></div>
+    panel_html_posts = """</span></div>
 			<div class=\"list-group\" id=\"{mailname}-board\">"""
 
-	panel_html_end = """
+    panel_html_end = """
 			</div>
         </div>"""
 
-	while True:
-		row = curs.fetchone()
+    while True:
+        row = curs.fetchone()
 
-		if row == None:
-			return ["\n".join(boards_1), "\n".join(boards_2)]
+        if row == None:
+            return ["\n".join(boards_1), "\n".join(boards_2)]
 
-		boardId = row['boardId']
-		[posts, numposts] = displayPosts(boardId, conn)
+        boardId = row['boardId']
+        [posts, numposts] = displayPosts(boardId, conn)
 
-		board_html = (panel_html_heading + str(numposts) + panel_html_posts).format(**row)
-		board_html += posts
-		board_html += panel_html_end
+        board_html = (
+            panel_html_heading + str(numposts) + panel_html_posts).format(**row)
+        board_html += posts
+        board_html += panel_html_end
 
-		if (boards_printed < math.ceil(total_boards/2)):
-			boards_1.append(board_html)
+        if (boards_printed < math.ceil(total_boards / 2)):
+            boards_1.append(board_html)
 
-		else:
-			boards_2.append(board_html)
+        else:
+            boards_2.append(board_html)
 
-		boards_printed += 1
+        boards_printed += 1
 
 
 # TODO: enable pagination for long pages of posts.
 # TODO: display timestamps at some point. Still trying to figure out 
 # how to do so.
 def displayPosts(boardId, conn):
-	curs = conn.cursor(MySQLdb.cursors.DictCursor)
-	data = (boardId,)
-	curs.execute("select * from form where type='post' and boardId= %s ", data)
-	numposts = curs.rowcount
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    data = (boardId,)
+    curs.execute("select * from form where type='post' and boardId= %s ", data)
+    numposts = curs.rowcount
 
-	isFirst = True
-	posts = []
-	
-	start_post_active = """<a href="#" class="list-group-item active">"""
-			
-	start_post = """<a href="#" class="list-group-item">"""
+    isFirst = True
+    posts = []
 
-	post_html = """
+    start_post_active = """<a href="#" class="list-group-item active">"""
+
+    start_post = """<a href="#" class="list-group-item">"""
+
+    post_html = """
 			<h4 class="list-group-item-heading">{title}</h4>
 			<p>{content}</p>
 			<h4><small>"""
-	
-	end_post = "</small></h4> </a>"
 
-	while True:
-		row = curs.fetchone()
+    end_post = "</small></h4> </a>"
 
+    while True:
+        row = curs.fetchone()
 
-		if row == None:
-			return ["\n".join(posts), numposts]
+        if row == None:
+            return ["\n".join(posts), numposts]
 
-		postId = row['formId']
+        postId = row['formId']
 
-		post_tags = displayTags(postId, conn)
+        post_tags = displayTags(postId, conn)
 
-		if isFirst:
-			posts.append(start_post_active + post_html.format(**row) + post_tags + end_post)
-			isFirst = False
+        image = display_image(conn, postId)
 
-		else:
-			posts.append(start_post + post_html.format(**row) + post_tags + end_post)
+        if isFirst:
+            posts.append(start_post_active + post_html.format(
+                **row) + post_tags + end_post)
+            isFirst = False
+
+        else:
+            posts.append(
+                start_post + post_html.format(**row) + image + "<br>"
+                + post_tags + end_post)
 
 
 def displayTags(postId, conn):
-	curs = conn.cursor(MySQLdb.cursors.DictCursor)
-	curs.execute("select * from tag where postId=%s", (postId,))
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute("select * from tag where postId=%s", (postId,))
 
-	tags = ""
+    tags = ""
 
-	while True:
-		row = curs.fetchone()
+    while True:
+        row = curs.fetchone()
 
-		if row == None:
-			return tags
+        if row == None:
+            return tags
 
-		tags += "#" + row['value'].lower().strip() + " "
+        tags += "#" + row['value'].lower().strip() + " "
 
 
 def addBoard(name, privacy_level, category):
-	conn = dbconn.connect(DSN)
-	
-	curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    conn = dbconn.connect(DSN)
 
-	mailname = name.strip().lower().replace(" ", "-")
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
 
-	curs.execute("select * from board where mailname=%s", (mailname,))
-	row = curs.fetchone()
+    mailname = name.strip().lower().replace(" ", "-")
 
-	if row == None:
-		curs.execute("insert into board (name, mailname, type, privacyLevel, category) values (%s, %s, 'board', %s, %s)", 
-			(name, mailname, privacy_level, category))
-		return "Board " + name + " created successfully."
+    curs.execute("select * from board where mailname=%s", (mailname,))
+    row = curs.fetchone()
 
-	else:
-		return "Board " + name + " already exists."
+    if row == None:
+        curs.execute(
+            "insert into board (name, mailname, type, privacyLevel, category) values (%s, %s, 'board', %s, %s)",
+            (name, mailname, privacy_level, category))
+        return "Board " + name + " created successfully."
+
+    else:
+        return "Board " + name + " already exists."
 
 
-def addPost(boards, subject, message, tags):
-	conn = dbconn.connect(DSN)
-	
-	curs = conn.cursor(MySQLdb.cursors.DictCursor)
+def addPost(boards, subject, message, tags, image):
+    conn = dbconn.connect(DSN)
 
-	# For feedback.
-	sent = ""
-	failed_to_send = ""
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
 
-	for board in boards:
-		mailname = board.strip().lower().replace(" ", "-")
-		#print 'Searching for board ' + mailname
-		curs.execute("select boardId from board where mailname=%s", (mailname,))
-		board_row = curs.fetchone()
-		if board_row != None:
-			boardId = board_row['boardId']
+    # For feedback.
+    sent = ""
+    failed_to_send = ""
 
-			# I can't get the created timestamp from mysql without causing some timestamp issues later on, 
-			# so I have to calculate it myself in python beforehand.
-			current_time = str(datetime.now())
+    for board in boards:
+        mailname = board.strip().lower().replace(" ", "-")
+        #print 'Searching for board ' + mailname
+        curs.execute("select boardId from board where mailname=%s",
+                     (mailname,))
+        board_row = curs.fetchone()
+        if board_row != None:
+            boardId = board_row['boardId']
 
-			curs.execute("insert into form (boardId, created, title, content, type) values (%s, %s, %s, %s, 'post')", 
-				(boardId, current_time, subject, message))
+            # I can't get the created timestamp from mysql without causing some timestamp issues later on,
+            # so I have to calculate it myself in python beforehand.
+            current_time = str(datetime.now())
 
-			addTags(boardId, current_time, tags, conn)
+            curs.execute(
+                "insert into form (boardId, created, title, content, type) values (%s, %s, %s, %s, 'post')",
+                (boardId, current_time, subject, message))
 
-			sent += board + ","
+            addTags(boardId, current_time, tags, conn)
 
-		else:
-			failed_to_send += board + ","
+            add_image(conn, boardId, current_time, image.filename, image.file)
 
-	if failed_to_send != "":
-		unsent = "Post could not be sent to " + failed_to_send.rstrip(",")
-	else:
-		unsent = ""
+            sent += board + ","
 
-	return "Post sent to " + sent.rstrip(",") + "<br>" + unsent
+        else:
+            failed_to_send += board + ","
+
+    if failed_to_send != "":
+        unsent = "Post could not be sent to " + failed_to_send.rstrip(",")
+    else:
+        unsent = ""
+
+    return "Post sent to " + sent.rstrip(",") + "<br>" + unsent
 
 
 def addTags(boardId, current_time, tags, conn):
-	curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
 
-	curs.execute("select formId from form where boardId=%s and created=%s and type='post'", 
-				(boardId, current_time))
+    curs.execute(
+        "select formId from form where boardId=%s and created=%s and type='post'",
+        (boardId, current_time))
 
-	post_row = curs.fetchone()
-	if post_row != None:
-		postId = post_row['formId']
+    post_row = curs.fetchone()
+    if post_row is not None:
+        postId = post_row['formId']
 
-	for tag in tags:
-		standardized_tag = tag.lower().strip()
-		curs.execute("insert into tag (postId, value) values (%s, %s)", 
-			(postId, standardized_tag))
+    for tag in tags:
+        standardized_tag = tag.lower().strip()
+        curs.execute("insert into tag (postId, value) values (%s, %s)",
+                     (postId, standardized_tag))
 
 
 def main():
-	names = getBoardNames()
-	[boards_col1, boards_col2] = displayBoards()
-	#addPost(boards=['tower', ' claflin', 'Cs department  '], subject='High tea tonight', message='Come to tower high tea tonight at 8:30pm')
-	return names, boards_col1, boards_col2
+    names = getBoardNames()
+    [boards_col1, boards_col2] = displayBoards()
+    #addPost(boards=['tower', ' claflin', 'Cs department  '], subject='High tea tonight', message='Come to tower high tea tonight at 8:30pm')
+    return names, boards_col1, boards_col2
+
 
 if __name__ == "__main__":
-	for each in main():
-		print each
-	#main()
+    for each in main():
+        print each
+    #main()
 
 
 
