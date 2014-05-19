f500db0d482f7887e9b5a0875631ca712efb6f90
Caroline Gallagher <cgallagher@wellesley.edu>
2014-05-19 17:21:18 -0400

    Made sql file to display all tables neatly

---
+++ describe_tables.sql
@@ -0,0 +1,15 @@
+use neighbrd_db;
+
+show columns from user;
+
+show columns from board;
+
+show columns from form;
+
+show columns from tag;
+
+show columns from picfile;
+
+show columns from usersessions;
+
+show columns from signature;
